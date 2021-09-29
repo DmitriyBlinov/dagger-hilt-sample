0b7bbe9db296a022bb631af4cc851522a547b73b
Dima <dmitriy.ev.blinov@gmail.com>
2021-09-29 15:21:06 +0700

    First commit with VM

---
+++ app/src/main/java/com/example/daggerhilt/AppModule.kt
@@ -0,0 +1,18 @@
+package com.example.daggerhilt
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    @Named("String1")
+    fun provideTestString() = "This is a string we will inject"
+}
